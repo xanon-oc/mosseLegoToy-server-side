6badcda7bf5af5819035a489b3c3ed1ba8bd7a6a
NILOY ROY <121684663+xanon-oc@users.noreply.github.com>
2023-05-18 15:12:48 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// mongodb
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const uri = "mongodb://0.0.0.0:27017";
+// const uri = `mongodb+srv://${process.env.MOOSE_DATA_UI}:${process.env.MOOSE_DATA_UP}@cluster0.25nqiwd.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const productCollections = client.db("mooseDb").collection("products");
+
+    // all data
+
+    app.get("/all-products", async (req, res) => {
+      const result = await productCollections.find().toArray();
+      res.send(result);
+    });
+
+    // post
+
+    app.post("/postProduct", async (req, res) => {
+      const body = req.body;
+      const result = await productCollections.insertMany(body);
+      console.log(result);
+      res.send(result);
+    });
+
+    // delete
+
+    app.delete("/deleteProduct/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollections.deleteOne(query);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("The Moose Server Is Running");
+});
+
+app.listen(port, () => {
+  console.log(`Server is hitting on port : ${port} `);
+});
